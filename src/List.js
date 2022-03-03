45066c0906590636bfb7914d394ffb08f832d488
Mouez98 <mouez.matoui@gmail.com>
2022-03-03 12:21:54 +0100

    local storage

--- src/List.js
+++ src/List.js
@@ -6,19 +6,27 @@ const List = ({ items, removeItem, editItem }) => {
   return (
     <article className="grocery-container">
       {items.map((item) => {
-        const {id, title} = item;
+        const { id, title } = item;
         return (
           <div className="grocery-item" key={id}>
             <p className="title">{title} </p>
             <div className="btn-container">
-              <button type="button" className="edit-btn" onClick={()=> editItem(id)}>
+              <button
+                type="button"
+                className="edit-btn"
+                onClick={() => editItem(id)}
+              >
                 <FaEdit />
               </button>
-              <button type="button" className="delete-btn" onClick={()=> removeItem(id)}>
+              <button
+                type="button"
+                className="delete-btn"
+                onClick={() => removeItem(id)}
+              >
                 <FaTrash />
               </button>
             </div>
-            <div className="delete-btn"></div>
+         
           </div>
         );
       })}
